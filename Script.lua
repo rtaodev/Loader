b12d2ce62a54983bfe8d0339a20e60ae71e8bdad
rtaodev <taowarut789@gmail.com>
2025-07-26 11:41:21 +0700

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,23 @@
+--[[
+RTaO_Dev All Script 
+]]
+
+repeat task.wait() until game.Players.LocalPlayer and game.Players.LocalPlayer.Character
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+print("Supported game!")
+local creator = game.CreatorId
+
+local games = {
+    [35289532] = '', -- Dig
+    [35888785] = '', -- Prospecting
+} 
+
+if games[creator] then 
+    print("Please wait, daddyhh~ loading..")
+    loadstring(game:HttpGet(games[creator]))()
+else
+    warn("Unsupported game.")
+end
