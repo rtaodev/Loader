6e6a9d1093451eafde1d6a05d8500b73b23d5477
rtaodev <taowarut789@gmail.com>
2025-09-29 00:50:04 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -31,6 +31,7 @@ local games = {
     [35888785] = 'https://raw.githubusercontent.com/rtaodev/rtao_exe/main/Prospecting.lua', -- Prospecting
     [6042520] = 'https://raw.githubusercontent.com/rtaodev/rtao_exe/main/99nights_rtaodev_v9.lua', -- 99 Nights in the Forest
     [35789249] = 'https://pandadevelopment.net/virtual/file/b02a8b6346ea8b36', -- Grow a Garden
+    [7381705] = 'https://raw.githubusercontent.com/rtaodev/rtao_exe/main/Fisch_ByRTaO.lua', -- Fisch
 } 
 
 if games[creator] then 
