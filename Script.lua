8ed4b8eb6ebface2369f8b7b7af0807626ad266d
rtaodev <taowarut789@gmail.com>
2025-10-08 02:41:30 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -26,7 +26,7 @@ local games = {
     [6042520] = 'https://raw.githubusercontent.com/rtaodev/rtao_exe/main/99Nights_BETA_1.1.lua', -- 99 Nights in the Forest
     [35789249] = 'https://pandadevelopment.net/virtual/file/b02a8b6346ea8b36', -- Grow a Garden
     [7381705] = 'https://raw.githubusercontent.com/rtaodev/rtao_exe/main/Fisch_ByRTaO.lua', -- Fisch
-    [34869880] = 'https://raw.githubusercontent.com/rtaodev/rtao_exe/main/PVSB_V1.lua', --Plants Vs Brainrots
+    [34869880] = 'https://raw.githubusercontent.com/rtaodev/rtao_exe/main/PVSB_K1.lua', --Plants Vs Brainrots
 } 
 
 if games[creator] then 
